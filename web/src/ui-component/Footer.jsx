bc001abf503ca0ae96152660be47905570631319
harrisonhxy <harrisonhxyang@gmail.com>
2025-04-05 16:02:49 +0800

    Change footer

--- web/src/ui-component/Footer.jsx
+++ web/src/ui-component/Footer.jsx
@@ -1,38 +1,54 @@
 // material-ui
-import { Link, Container, Box } from '@mui/material';
+import { Link as MuiLink, Container } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { styled } from '@mui/material/styles';
 
-// ==============================|| FOOTER - AUTHENTICATION 2 & 3 ||============================== //
+// 创建一个样式化的footer组件
+const StyledFooter = styled('footer')(({ theme }) => ({
+  textAlign: 'center',
+  fontSize: '14px',
+  color: '#555',
+  padding: '5px 0',
+  '& p': {
+    margin: '5px 0',
+  },
+  '& a': {
+    color: '#007bff',
+    textDecoration: 'none',
+    marginRight: '5px',
+    '&:hover': {
+      textDecoration: 'underline',
+      color: '#0056b3',
+    }
+  },
+  '& .powered': {
+    display: 'inline-block',
+    marginLeft: '5px',
+  }
+}));
+
+// ==============================|| FOOTER ||============================== //
 
 const Footer = () => {
   const siteInfo = useSelector((state) => state.siteInfo);
   const { t } = useTranslation();
 
   return (
-    <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '64px' }}>
-      <Box sx={{ textAlign: 'center' }}>
-        {siteInfo.footer_html ? (
-          <div className="custom-footer" dangerouslySetInnerHTML={{ __html: siteInfo.footer_html }}></div>
-        ) : (
-          <>
-            <Link href="https://github.com/MartialBE/one-hub" target="_blank">
-              {siteInfo.system_name} {import.meta.env.VITE_APP_VERSION}{' '}
-            </Link>
-            {t('footer.developedBy')}{' '}
-            <Link href="https://github.com/MartialBE" target="_blank">
-              MartialBE
-            </Link>
-            ，{t('footer.basedOn')}{' '}
-            <Link href="https://github.com/songquanpeng" target="_blank">
-              JustSong
-            </Link>{' '}
-            One Hub，{t('footer.sourceCode')}
-            <Link href="https://opensource.org/licenses/mit-license.php"> {t('footer.license')}</Link>
-          </>
-        )}
-      </Box>
+    <Container>
+      {siteInfo.footer_html ? (
+        <div className="custom-footer" dangerouslySetInnerHTML={{ __html: siteInfo.footer_html }}></div>
+      ) : (
+        <StyledFooter>
+          <p>
+            ©
+            <MuiLink href="https://harrizone.com" target="_blank">Harrizone.com</MuiLink>
+            <span className="powered">Powered by</span>
+            <MuiLink href="https://github.com/MartialBE/one-hub" target="_blank">One Hub</MuiLink>
+          </p>
+        </StyledFooter>
+      )}
     </Container>
   );
 };
